17e969488aa361e44fffcf1fcf9f346751971415
rspeicher <rspeicher@gmail.com>
2009-12-21 15:18:01 -0500

    Add Applicants controller.

---
+++ spec/controllers/applicants_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ApplicantsController do
+
+  #Delete this example and add some real ones
+  it "should use ApplicantsController" do
+    controller.should be_an_instance_of(ApplicantsController)
+  end
+
+end
