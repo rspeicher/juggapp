eb7684e6bae5e5fd7580ad3e18a51e279ba0d91c
rspeicher <rspeicher@gmail.com>
2009-12-21 19:10:36 -0500

    Add array of server names to Applicant.

--- spec/factories/applicant.rb
+++ spec/factories/applicant.rb
@@ -1,5 +1,9 @@
 Factory.define :applicant do |a|
   a.status 'new'
   a.user_id 1
+  a.character_server "Mal'Ganis"
   a.character_name 'Tsigo'
+  a.character_class 'Priest'
+  a.character_race 'Undead'
+  a.armory_link "http://www.wowarmory.com/character-sheet.xml?r=Mal%27Ganis&n=Tsigo"
 end
--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -53,8 +53,21 @@ describe Applicant do
     @applicant = Factory(:applicant)
   end
   
+  it "should be valid" do
+    @applicant.should be_valid
+  end
+  
   it { should allow_value('denied').for(:status) }
   it { should_not allow_value('invalid').for(:status) }
   it { should validate_presence_of(:user_id) }
+  it { should allow_value("Mal'Ganis").for(:character_server) }
+  it { should_not allow_value('Invalid').for(:character_server) }
   it { should validate_presence_of(:character_name) }
+  it { should allow_value('Priest').for(:character_class) }
+  it { should_not allow_value('Invalid').for(:character_class) }
+  it { should allow_value('Undead').for(:character_race) }
+  it { should_not allow_value('Invalid').for(:character_race) }
+  # it { should allow_value("http://www.wowarmory.com/character-sheet.xml?r=Mal%27Ganis&n=Tsigo").for(:armory_link) }
+  # it { should allow_value("http://wowarmory.com/character-sheet.xml?r=Mal%27Ganis&n=Tsigo").for(:armory_link) }
+  # it { should_not allow_value("http://www.google.com/").for(:armory_link) }
 end
