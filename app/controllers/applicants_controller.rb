0ceca450a65c7db274b498ae182d353dfbe002ab
rspeicher <rspeicher@gmail.com>
2009-12-21 19:11:07 -0500

    Index/Create/Update for Applicant.

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,2 +1,58 @@
 class ApplicantsController < ApplicationController
+  before_filter :require_user
+  
+  def index
+    if current_user.is_admin?
+      @applications = Applicant.find(:all, :order => 'updated_at DESC')
+    else
+      @applications = current_user.applications.find(:all, :order => 'updated_at DESC')
+    end
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def new
+    @applicant = Applicant.new
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def edit
+    @applicant = Applicant.find(params[:id])
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def create
+    @applicant = Applicant.new(params[:applicant])
+  
+    respond_to do |wants|
+      if @applicant.save
+        flash[:message] = 'Application submitted successfully.'
+        wants.html { redirect_to(@applicant) }
+      else
+        flash[:error] = 'Application failed, please see errors below.'
+        wants.html { render :action => "new" }
+      end
+    end
+  end
+  
+  def update
+    @applicant = Applicant.find(params[:id])
+    
+    respond_to do |wants|
+      if @applicant.update_attributes(params[:applicant])
+        flash[:success] = 'Application updated successfully.'
+        wants.html { redirect_to(applications_path) }
+      else
+        wants.html { render :action => 'edit' }
+      end
+    end
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+    $('#applicant_character_server').change(populateArmory);
+    $('#applicant_character_name').change(populateArmory);
+});
+
+function populateArmory() {
+    // if ($('#applicant_armory_link').val() != '') {
+    //     return false;
+    // }
+    
+    cserver = $('#applicant_character_server').val();
+    cname   = $('#applicant_character_name').val();
+    
+    $('#applicant_armory_link').val("http://www.wowarmory.com/character-sheet.xml?r=" + cserver + "&n=" + cname);
+    $('#applicant_armory_link').effect('highlight', {}, 1500);
+}
