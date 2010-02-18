5d995344c76aa766411c42a81da6c41d81113302
rspeicher <rspeicher@gmail.com>
2010-02-18 06:25:57 -0500

    Get rid of factories folder, just use a single file.

--- spec/factories/applicant.rb
+++
@@ -1,9 +0,0 @@
-Factory.define :applicant do |a|
-  a.status 'new'
-  a.user_id 1
-  a.character_server "Mal'Ganis"
-  a.character_name 'Tsigo'
-  a.character_class 'Priest'
-  a.character_race 'Undead'
-  a.armory_link "http://www.wowarmory.com/character-sheet.xml?r=Mal%27Ganis&n=Tsigo"
-end
