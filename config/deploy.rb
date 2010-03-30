0f237538a5096f8f654bd52af1dcadb45de1ca65
rspeicher <rspeicher@gmail.com>
2010-03-30 01:47:57 -0400

    Add deploy tasks.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,45 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :stages, %w(production staging)
+set :default_stage, 'production'
+require 'capistrano/ext/multistage'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "juggapp"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :use_sudo, false
+set :keep_releases, 3
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :user, 'tsigo'
+set :ssh_options, { :forward_agent => true, :keys => "/Users/tsigo/.ssh/id_rsa" }
 
-# namespace :deploy do
-#   task :start {}
-#   task :stop {}
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
+namespace :deploy do
+  desc "Start the server"
+  task :start, :roles => :app do
+    run "rm -f #{current_release}/tmp/stop.txt"
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  desc "Redirect to a 503 file (stop the server)"
+  task :stop, :roles => :app do
+    run "touch #{current_release}/tmp/stop.txt"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
+
+after "deploy:symlink", "deploy:database_yml"
+
+namespace :deploy do
+  desc "Copy database.yml file"
+  task :database_yml, :roles => :db do
+    run "cp #{shared_path}/config/database.yml #{current_release}/config/database.yml"
+    run "cp #{shared_path}/config/juggernaut.yml #{current_release}/config/juggernaut.yml"
+  end
+end
+
+# Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+#   $: << File.join(vendored_notifier, 'lib')
 # end
+#
+# require 'hoptoad_notifier/capistrano'
---
+++ config/deploy/production.rb
@@ -0,0 +1,35 @@
+set :domain,      "apply.juggernautguild.com"
+set :repository,  "git@tsigo.com:#{application}.git"
+set :scm,         'git'
+set :branch,      'master'
+
+set :deploy_to,   "/var/www/rails/#{application}"
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+before "deploy:update", "deploy:check_revision"
+
+namespace :deploy do
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    # TODO: Crontab for updating servers!
+    # run "cd #{release_path} && whenever --update-crontab #{application}"
+  end
+
+  desc "Make sure there is something to deploy"
+  task :check_revision, :roles => [:web] do
+    unless `git rev-parse HEAD` == `git rev-parse production/master`
+      puts ""
+      puts "  \033[1;33m******************************************************\033[0m"
+      puts "  \033[1;33m* WARNING: HEAD is not the same as production/master *\033[0m"
+      puts "  \033[1;33m******************************************************\033[0m"
+      puts ""
+
+      exit
+    end
+  end
+end
+
+after "deploy:symlink", "deploy:update_crontab"
