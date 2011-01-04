692cdf0b7d2604c693b44474cf919e9d822cc188
rspeicher <rspeicher@gmail.com>
2011-01-04 12:05:37 -0500

    Fix URL for servers with spaces in their name

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,7 @@ $(function() {
 });
 
 function populateArmory() {
-    cserver = $('#applicant_server_id :selected').text().replace(/[^A-Za-z0-9]/, '').toLowerCase();
+    cserver = $('#applicant_server_id :selected').text().replace(/\s/, '-').replace(/[^A-Za-z0-9\-]/, '').toLowerCase();
     cname   = $('#applicant_character_name').val().toLowerCase();
 
     $('#applicant_armory_link').val("http://us.battle.net/wow/en/character/" + cserver + "/" + cname + "/advanced");
